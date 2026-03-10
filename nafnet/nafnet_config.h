891bc70c314f1266eed44b73f93f49cc1ca73644
heyi-wang <ge59xey@mytum.de>
2026-03-10 09:32:26 +0100

    Add files via upload

---
+++ nafnet/nafnet_config.h
@@ -0,0 +1,10 @@
+#ifndef NAFNET_CONFIG_H
+#define NAFNET_CONFIG_H
+#define NUM_IMAGES 10
+#define CROP_SIZE 64
+#define IMG_CHANNELS 3
+#define WIDTH 32
+static const int enc_blk_nums[4] = {2, 2, 4, 8};
+static const int dec_blk_nums[4] = {2, 2, 2, 2};
+#define MIDDLE_BLK_NUM 12
+#endif
